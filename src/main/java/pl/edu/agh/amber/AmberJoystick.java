0908d368bc314dc21af904beebccd7ed44f39c7c
Paweł Suder <pawel@suder.info>
2015-10-13 18:24:25 +0200

    Refactor mechanism of execution, add shutdown hook, add libraries

--- src/main/java/pl/edu/agh/amber/AmberJoystick.java
+++ src/main/java/pl/edu/agh/amber/AmberJoystick.java
@@ -9,100 +9,87 @@ import java.io.IOException;
 
 public class AmberJoystick implements JoystickListener, Runnable {
 
-    private float x;
-    private float y;
-    private boolean stop = true;
+    private final int max_speed;
+    private final AmberClient client;
+    private final RoboclawProxy roboclawProxy;
 
-    public static void main(String[] args) {
-        (new AmberJoystick()).runDemo(args[0], Integer.parseInt(args[1]), Integer.parseInt(args[2]));
+    private boolean stop = false;
+
+    public static void main(String[] args) throws IOException, InterruptedException {
+        new AmberJoystick(args[0], Integer.parseInt(args[1]), Integer.parseInt(args[2]));
     }
 
-    public void runDemo(String address, int joy, int max_speed) {
+    public AmberJoystick(String address, int joy, int max_speed) throws IOException {
+        this.max_speed = max_speed;
+        client = new AmberClient(address, 26233);
+        roboclawProxy = new RoboclawProxy(client, 0);
+        Runtime.getRuntime().addShutdownHook(new Thread(this));
 
-        try {
-            Joystick joystick = Joystick.createInstance(joy);
-            joystick.addJoystickListener(this);
-        } catch (IOException e1) {
-            e1.printStackTrace();
+        Joystick joystick = Joystick.createInstance(joy);
+        joystick.addJoystickListener(this);
+    }
+
+    @Override
+    public synchronized void joystickAxisChanged(Joystick joystick) {
+        float x = joystick.getX();
+        float y = joystick.getY();
+
+        float lSpeed;
+        float rSpeed;
+        float lAsym;
+        float rAsym;
+
+        int motorsLeftSpeed;
+        int motorsRightSpeed;
+
+        if (x < 0) {
+            lAsym = -Math.abs(x);
+            rAsym = Math.abs(x);
+        } else {
+            lAsym = Math.abs(x);
+            rAsym = -Math.abs(x);
         }
 
-        AmberClient client;
+        lSpeed = -y;
+        rSpeed = -y;
+
+        lSpeed += lAsym;
+        rSpeed += rAsym;
+
+        if (Math.abs(lSpeed) > 1.0) {
+            lSpeed = Math.signum(lSpeed);
+        }
+
+        if (Math.abs(rSpeed) > 1.0) {
+            rSpeed = Math.signum(rSpeed);
+        }
+
+
+        if (stop || (Math.abs(rSpeed) <= 0.001 && Math.abs(lSpeed) <= 0.001)) {
+            motorsLeftSpeed = 1;
+            motorsRightSpeed = 1;
+        } else {
+            motorsRightSpeed = (int) (rSpeed * max_speed);
+            motorsLeftSpeed = (int) (lSpeed * max_speed);
+        }
+
+        System.err.println(motorsLeftSpeed + ", " + motorsRightSpeed);
         try {
-            client = new AmberClient(address, 26233);
-
-            RoboclawProxy roboclawProxy = new RoboclawProxy(client, 0);
-
-            float lSpeed;
-            float rSpeed;
-            float lAsym;
-            float rAsym;
-
-            int motorsLeftSpeed;
-            int motorsRightSpeed;
-
-            while (true) {
-
-                synchronized (this) {
-
-                    if (x < 0) {
-                        lAsym = -Math.abs(x);
-                        rAsym = Math.abs(x);
-                    } else {
-                        lAsym = Math.abs(x);
-                        rAsym = -Math.abs(x);
-                    }
-
-                    lSpeed = -y;
-                    rSpeed = -y;
-
-                    lSpeed += lAsym;
-                    rSpeed += rAsym;
-
-                    if (Math.abs(lSpeed) > 1.0) {
-                        lSpeed = Math.signum(lSpeed);
-                    }
-
-                    if (Math.abs(rSpeed) > 1.0) {
-                        rSpeed = Math.signum(rSpeed);
-                    }
-
-
-                    if (stop || (Math.abs(rSpeed) <= 0.2 && Math.abs(lSpeed) <= 0.2)) {
-                        motorsLeftSpeed = 1;
-                        motorsRightSpeed = 1;
-                    } else {
-                        motorsRightSpeed = (int) (rSpeed * max_speed);
-                        motorsLeftSpeed = (int) (lSpeed * max_speed);
-                    }
-                }
-
-                roboclawProxy.sendMotorsCommand(motorsLeftSpeed, motorsRightSpeed, motorsLeftSpeed, motorsRightSpeed);
-                Thread.sleep(10);
-            }
-
-
+            roboclawProxy.sendMotorsCommand(motorsLeftSpeed, motorsRightSpeed, motorsLeftSpeed, motorsRightSpeed);
         } catch (IOException e) {
-            System.err.println("Connection error: " + e);
-        } catch (Exception e) {
             e.printStackTrace();
         }
     }
 
     @Override
-    public synchronized void joystickAxisChanged(Joystick joystick) {
-        x = joystick.getX();
-        y = joystick.getY();
-
-    }
-
-    @Override
     public void joystickButtonChanged(Joystick arg0) {
-        if (arg0.isButtonDown(0)) {
+        if (arg0.isButtonDown(1)) {
             stop = !stop;
         }
     }
 
     @Override
     public void run() {
+        client.terminate();
     }
 }
